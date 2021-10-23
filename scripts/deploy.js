2ae4821a035592ef66ff544639a74ad99a3075f0
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 14:59:15 -0300

    Update deploy script to pass in default characters when deploying

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("Game");
+  const gameContract = await gameContractFactory.deploy(
+    ["Devil Doggy", "Sleepy Cat", "Fairy Godmother Mouse"],
+    [
+      "https://www.fineartstorehouse.com/p/629/jack-russell-devil-dog-19654897.jpg.webp",
+      "https://www.meme-arsenal.com/memes/1ff1e9812d9087a92f60cf4ae61202fd.jpg",
+      "https://www.lonewolfforest.com/uploads/7/4/0/3/7403404/4595836_orig.jpg",
+    ],
+    [200, 100, 300],
+    [50, 100, 30]
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
