5a256c32068036d54e03f318ab6fb77460e827e7
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 16:06:48 -0300

    Set up rinkeby network and dotenv env variables in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -18,4 +20,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.RINKEBY_ACCOUNT_KEY,]
+    },
+  },
 };
