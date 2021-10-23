9e8e47da6dfcd43bee91945dd558917237fb1fb2
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 14:36:09 -0300

    Create Character struct and characters array in storage

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -5,6 +5,17 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Game {
+    struct Character {
+        uint256 index;
+        string name;
+        uint256 hp;
+        uint256 maxHp;
+        string imageURI;
+        uint256 attackDamage;
+    }
+
+    Character[] characters;
+
     constructor() {
         console.log("Hello world");
     }
