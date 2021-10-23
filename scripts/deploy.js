5724ea4e6ef1ea63c70a844c5485e4fac35bf45c
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 17:35:32 -0300

    Pass in boss data when deploying in deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,18 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("Game");
   const gameContract = await gameContractFactory.deploy(
-    ["Devil Doggy", "Sleepy Cat", "Fairy Godmother Mouse"],
+    ["Evil Doggy", "Sleepy Cat", "Fairy Godmother Mouse"],
     [
       "https://www.fineartstorehouse.com/p/629/jack-russell-devil-dog-19654897.jpg.webp",
       "https://www.meme-arsenal.com/memes/1ff1e9812d9087a92f60cf4ae61202fd.jpg",
       "https://www.lonewolfforest.com/uploads/7/4/0/3/7403404/4595836_orig.jpg",
     ],
     [200, 100, 300],
-    [50, 100, 30]
+    [50, 100, 30],
+    "Bearish Birdie",
+    "https://i.ytimg.com/vi/ghnloXl4Jx4/maxresdefault.jpg",
+    10000,
+    50
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
@@ -29,6 +33,12 @@ const main = async () => {
   txn = await gameContract.mintNFT(2);
   await txn.wait();
   console.log("Minted NFT #4");
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
 
   console.log("Done deploying and minting!");
 };
